105f48eec800cdd343a9103b84d81c2dbcab4025
jmacgregor <jimmy.macgregor@futurenet.com>
2021-06-22 19:21:03 +0100

    Brief amount about paradigms

--- src/ABC/index.js
+++ src/ABC/index.js
@@ -7,6 +7,7 @@ const AlgorithmBookClub = () => <div>
     <p>We covered a variety of topics, ending up discussing the neural network variations that were of interest at the time; ultimately working our way towards <Link href="https://en.wikipedia.org/wiki/Generative_adversarial_network">GANs</Link>.</p>
     <p>Along the way we investigated compression algorithms and cryptographic, but mostly we kept our focus on a standard subset of algorithms.</p>
     <p>Each week, we chose an area for investigation (for example, 'greedy algorithms' or 'genetic algorithms'), tried to write some implementation of a relevant algorithm, and had a look into any relevant performance characteristics in time and space.</p>
+    <p>We also often discussed the paradigms that particular algorithms often belong to, and many contributors including myself provided functional examples of algorithms traditionally presented as imperative, and the trade-offs incurred by using a higher-level paradigm.</p>
     <p>Most of the projects we created for this club have since disappeared in various ways, but any of my presentations that I recover in a usable form I have shared on this site.</p>
     <DefinitionList header="Algorithm Bookclub Demos" entries={[
         <span>Diffusion Reaction Algorithm on a Sphere: <Link href="https://drive.google.com/file/d/1Z7faiROIxpw-1PjMh2zV_1SgY_42Dmtc/view?usp=sharing">Mac OS</Link></span>,
