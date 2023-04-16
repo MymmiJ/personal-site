2f3e618bc17c8955c10cf9d8dfd76d54a0507270
jmacgregor <jimmy.macgregor@futurenet.com>
2023-04-16 16:14:00 +0100

    fixup: Allow time controls to be input in new skill group forms

--- src/RocketLeagueProgressTracker/Forms/SkillForm.js
+++ src/RocketLeagueProgressTracker/Forms/SkillForm.js
@@ -1,17 +1,24 @@
 import { Divider } from "@material-ui/core";
 import { MeasurementsForm } from "./MeasurementsForm";
 import { SkillFormElement } from "./SkillFormComponents/SkillFormElement";
+import { ROCKET_LEAGUE_MEASUREMENT_DISPLAY_OPTIONS } from "../Factories/measurementsMaker";
+import { TimeInput } from "../ProgressTracker/SkillInput/TimeInput";
+import { NumberInput } from "../ProgressTracker/SkillInput/NumberInput";
 
 export const SkillForm = ({ skill, updateSkill }) => {
+    const InputComponent = skill.measurements.display === ROCKET_LEAGUE_MEASUREMENT_DISPLAY_OPTIONS.TIME ?
+        TimeInput :
+        NumberInput;
+
     return skill ?
     <>
         <SkillFormElement
             {...{ skill, updateSkill }}
             field="name"
         />
-        <SkillFormElement
-            {...{ skill, updateSkill }}
-            field="degree"
+        <InputComponent
+            degree={ skill.degree }
+            onChange={ (degree) => updateSkill({ ...skill, degree }) }
         />
         <MeasurementsForm
             measurements={skill.measurements}
--- src/RocketLeagueProgressTracker/ProgressTracker/SkillInput/NumberInput.js
+++ src/RocketLeagueProgressTracker/ProgressTracker/SkillInput/NumberInput.js
@@ -1,10 +1,32 @@
-import { Input } from "@material-ui/core";
+import { FormControl, Input, InputLabel } from "@material-ui/core";
+import { Label } from "@material-ui/icons";
 
 export const NumberInput = ({ degree, onChange, onKeyUp = () => {} }) => {
-    return <Input
-        style={{ maxWidth: '128px', marginRight: '4px', marginLeft: '4px' }}
-        value={degree}
-        onChange={({ target: { value } }) => onChange(value)}
-        onKeyUp={onKeyUp}
-    />;
+    return <FormControl>
+        <InputLabel htmlFor={`number-input-${degree}`}><Label/></InputLabel>
+        <Input
+            id={`number-input-${degree}`}
+            style={{
+                maxWidth: '128px',
+                marginRight: '4px',
+                marginLeft: '4px',
+                ...(!!degree ? {
+                    backgroundImage: `linear-gradient(
+                        45deg, 
+                        rgba(0,0,0,0.5) 25%, 
+                        transparent 25%, 
+                        transparent 50%, 
+                        rgba(0,0,0,0.5) 50%, 
+                        rgba(0,0,0,0.5) 75%, 
+                        transparent 75%, 
+                        transparent
+                        )`,
+                    backgroundSize: `4px 4px`
+                } : {})
+            }}
+            value={degree}
+            onChange={({ target: { value } }) => onChange(value)}
+            onKeyUp={onKeyUp}
+        />
+    </FormControl>;
 }
